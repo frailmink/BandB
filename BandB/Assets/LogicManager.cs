f2200d173bfe39fc131d7f661f04c98fbdfbb03a
frailmink <adrismithgon@gmail.com>
2023-08-29 19:49:22 +0100

    made it possible to spawn tiles in the game

--- BandB/Assets/LogicManager.cs
+++ BandB/Assets/LogicManager.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.InputSystem;
 
 public class LogicManager : MonoBehaviour
 {
+    public Tilemap map;
+    public TileBase tile;
+
+    private Vector2 mWorldPos;
+    private Vector2 mPos;
+
     void Awake()
     {
         Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        mPos = Mouse.current.position.ReadValue();
+        mWorldPos = Camera.main.ScreenToWorldPoint(mPos);
+
+        Vector3Int temp = map.WorldToCell(mWorldPos);
+
+        map.SetTile(temp, tile);
     }
 }
