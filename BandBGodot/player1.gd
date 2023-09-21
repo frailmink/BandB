c01af38c2406e32e724cb441c23908d2309fa751
arthurbr111 <arthurbr1112004@gmail.com>
2023-09-21 21:34:09 +0100

    ADDed moovement on GOdot for second player

--- BandBGodot/player1.gd
+++ BandBGodot/player1.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var CanDoubleJump : bool = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,14 +13,20 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		CanDoubleJump = true
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Keyboard_Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("Keyboard_Jump") and CanDoubleJump == true:
+		velocity.y = JUMP_VELOCITY
+		CanDoubleJump = false
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Keyboard_Left", "Keyboard_Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
--- BandBGodot/player2.gd
+++ BandBGodot/player2.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
+const SPEED = 1000.0
+const JUMP_VELOCITY = -500.0
+var CanDoubleJump : bool = true
+var gravoty = 1200 
+var VEL = Vector2()
+var SLPIPYYYY = 10
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,18 +16,25 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
-
+		velocity.y += gravoty * delta
+	else:
+		CanDoubleJump = true
+		
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Gamepad_Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("Gamepad_Jump") and CanDoubleJump == true:
+		velocity.y = JUMP_VELOCITY
+		CanDoubleJump = false
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Gamepad_Left", "Gamepad_Right")
 	if direction:
-		velocity.x = direction * SPEED
+		VEL = lerp(velocity, Vector2(SPEED * direction, 0), 0.01)
+		velocity.x = VEL.x
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 30)
 
 	move_and_slide()
